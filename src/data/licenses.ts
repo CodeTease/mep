d916478ddd90e3fbe7b37930b09697bbd935112e
irossmini <irossmini@gmail.com>
2026-02-02 03:37:28 +0700

    feat: add more licenses

--- src/data/licenses.ts
+++ src/data/licenses.ts
@@ -48,5 +48,61 @@ export const POPULAR_LICENSES: License[] = [
         permissions: ['Commercial use', 'Modification', 'Distribution', 'Private use'],
         conditions: [],
         limitations: ['Liability', 'Warranty']
+    },
+    {
+        id: 'BSD-2-Clause',
+        name: 'BSD 2-Clause "Simplified" License',
+        description: 'A permissive license that is similar to the MIT License, with conditions requiring preservation of copyright and license notices.',
+        permissions: ['Commercial use', 'Modification', 'Distribution', 'Private use'],
+        conditions: ['License and copyright notice'],
+        limitations: ['Liability', 'Warranty']
+    },
+    {
+        id: 'LGPL-3.0',
+        name: 'GNU Lesser General Public License v3.0',
+        description: 'A weak copyleft license permitting proprietary use. Requires preservation of copyright and license notices. Modifications of the library under the same license must be provided under the LGPL, but applications built on top may be licensed proprietary.',
+        permissions: ['Commercial use', 'Modification', 'Distribution', 'Patent use', 'Private use'],
+        conditions: ['Disclose source', 'License and copyright notice', 'Same license', 'State changes'],
+        limitations: ['Liability', 'Warranty']
+    },
+    {
+        id: 'AGPL-3.0',
+        name: 'GNU Affero General Public License v3.0',
+        description: 'A strong copyleft license that requires releasing source code when distributing the software over a network. Includes all conditions of GPL-3.0 plus a network use clause.',
+        permissions: ['Commercial use', 'Modification', 'Distribution', 'Patent use', 'Private use'],
+        conditions: ['Disclose source', 'License and copyright notice', 'Network use disclosure', 'Same license', 'State changes'],
+        limitations: ['Liability', 'Warranty']
+    },
+    {
+        id: 'MPL-2.0',
+        name: 'Mozilla Public License 2.0',
+        description: 'A copyleft license that is easy to comply with. You must make the source code for any of your modifications available under MPL-2.0, but you can combine the software with code under other licenses.',
+        permissions: ['Commercial use', 'Modification', 'Distribution', 'Patent use', 'Private use'],
+        conditions: ['Disclose source', 'License and copyright notice', 'Same license'],
+        limitations: ['Liability', 'Warranty', 'Trademark use']
+    },
+    {
+        id: 'EPL-2.0',
+        name: 'Eclipse Public License 2.0',
+        description: 'A copyleft license based on the Mozilla Public License, designed for programs and libraries. Modified code under the same license must be provided.',
+        permissions: ['Commercial use', 'Modification', 'Distribution', 'Patent use', 'Private use'],
+        conditions: ['Disclose source', 'License and copyright notice', 'Same license'],
+        limitations: ['Liability', 'Warranty']
+    },
+    {
+        id: 'CC0-1.0',
+        name: 'Creative Commons Zero v1.0 Universal',
+        description: 'A public domain dedication waiver that places works in the public domain. A work released under CC0 incurs no obligations and anyone may use the work in any way.',
+        permissions: ['Commercial use', 'Modification', 'Distribution', 'Private use'],
+        conditions: [],
+        limitations: ['Liability', 'Warranty']
+    },
+    {
+        id: 'WTFPL',
+        name: 'Do What The F* You Want To Public License',
+        description: 'An extremely permissive license that allows anyone to do whatever they want with the work with no restrictions whatsoever.',
+        permissions: ['Commercial use', 'Modification', 'Distribution', 'Private use'],
+        conditions: [],
+        limitations: ['Liability', 'Warranty']
     }
 ];
